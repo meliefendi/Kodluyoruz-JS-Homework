7e76f502e05ac844c4dbc0d004433aa5c159e0c0
Melih Akkas <melh_akkas@hotmail.com>
2022-11-13 19:06:34 +0300

    Update js.js

--- Sayıları arttırıp azaltma/js.js
+++ Sayıları arttırıp azaltma/js.js
@@ -10,6 +10,7 @@ domCounter.innerHTML = 0
 domCrease.addEventListener("click",clickEvent)
 domDegrease.addEventListener("click",clickEvent)
 
+-----------------------------------------------------1
 // function clickEvent(){
 //     console.log(this.id)
 //     if(this.id == "increase"){
@@ -18,19 +19,19 @@ domDegrease.addEventListener("click",clickEvent)
 //         domCounter.innerHTML = counter -= 1
 //     }
 // }
-
+-----------------------------------------------------2
 // function clickEvent(){
 //     console.log(this.id)
 //     this.id == "increase"? domCounter.innerHTML = counter += 1 :
 //         domCounter.innerHTML = counter -= 1
     
 // }
-
+-----------------------------------------------------3
 function clickEvent (){
     this.id == "increase" ? counter += 1 : counter -= 1; domCounter.innerHTML = counter
 }
 
-
+-----------------------------------------------------4
 domİncrease.addEventListener("click",function(){
     domCounter.innerHTML = counter += 1
 });
@@ -38,3 +39,22 @@ domİncrease.addEventListener("click",function(){
 domDecrease.addEventListener("click",function(){
     domCounter.innerHTML = counter -= 1
 })
+
+-----------------------------------------------------5
+    let counter = 0;
+
+updateDisplay();
+
+domİncrease.addEventListener("click",()=>{
+    counter++;
+    updateDisplay();
+}) ;
+
+domDecrease.addEventListener("click",()=>{
+    counter --;
+    updateDisplay();
+});
+
+function updateDisplay(){
+    domCounter.innerHTML = counter;
+};
