f39fc46fff9dda2bead637755ff5385f9b15b918
Melih Akkas <melh_akkas@hotmail.com>
2023-01-02 19:43:47 +0300

    Add files via upload

---
+++ DETAİL TODOLİST/script2.js
@@ -0,0 +1,65 @@
+const input = document.querySelector(".form-control");
+const buton = document.querySelector(".btn");
+
+buton.addEventListener("click", function () {
+    let inputValue = input.value;
+    if (inputValue.trim() != 0) {
+        
+        let taskList = JSON.parse(localStorage.getItem("tasks"))
+       
+        if (taskList === null) {
+            taskList = []
+        }
+        taskList.push(inputValue)
+        localStorage.setItem("tasks", JSON.stringify(taskList))
+    }
+    
+    displayItem()
+})
+
+function displayItem(){
+    let taskList = JSON.parse(localStorage.getItem("tasks"))
+    if(taskList === null){
+        taskList = []
+    }
+    let html = "";
+    let itemDisplay = document.querySelector(".list");
+    taskList.forEach((data, index) => {
+        
+        html += `
+     <li onClick ="toggle()" class="list-li mt-2">${data} &nbsp;<button class="deleteTask btn btn-danger" onClick="deleteItem(${index})"> <i class="far fa-trash-alt"></i></button>  </li> 
+        `
+    })
+    itemDisplay.innerHTML = html;
+}
+displayItem()
+
+function toggle() {
+    let elements = document.querySelectorAll(".list-li");
+
+elements.forEach(function(element) {
+  element.addEventListener("click", function() {
+    let taskList = JSON.parse(localStorage.getItem("tasks"));
+    element.classList.toggle("strike");
+    localStorage.setItem("tasks",JSON.stringify(taskList));
+  });
+});
+   
+
+}
+
+
+function deleteItem(index){
+    let taskList = JSON.parse(localStorage.getItem("tasks"))
+    taskList.splice(index,1)
+    localStorage.setItem("tasks",JSON.stringify(taskList));
+    displayItem()
+}
+
+function clearTask(){
+    localStorage.clear()
+    displayItem()
+}
+
+
+
